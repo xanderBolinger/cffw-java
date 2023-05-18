cf7869cb723c868e86c0b6507affceff2041fa75
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-05-17 23:43:10 -0600

    Update AllTests.java

--- CFFW/src/JUnitTests/AllTests.java
+++ CFFW/src/JUnitTests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CeSuppressionTests.class, CommandLineInterfaceTests.class, CorditeExpansionTests.class,
+@SuiteClasses({ CeSuppressionTests.class, /*CommandLineInterfaceTests.class,*/ CorditeExpansionTests.class,
 		DamageTests.class, ExcelUtililtyTests.class, GrenadeTests.class, InventoryTest.class, OpticTests.class,
 		PcDamageUtilityTests.class, ReloadTests.class, MedicalTests.class,  ShootTests.class})
 public class AllTests {
