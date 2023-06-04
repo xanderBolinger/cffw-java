a6d36a4d2c11e4e536a6577293638d8e752bf678
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-06-04 14:08:21 -0600

    c

--- CFFW/src/Trooper/Trooper.java
+++ CFFW/src/Trooper/Trooper.java
@@ -209,7 +209,7 @@ public class Trooper implements Serializable {
 	public Inventory inventory = new Inventory(this);
 	public int encumberanceModifier = 0;
 
-	public StatBlock ceStatBlock;
+	public transient StatBlock ceStatBlock;
 
 	public class MaximumSpeed implements Serializable {
 
