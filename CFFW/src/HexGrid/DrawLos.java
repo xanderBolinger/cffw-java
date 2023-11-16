e6187e83f94f93749af2422478ca328db7549b6e
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-11-15 20:06:52 -0700

    c

---
+++ CFFW/src/HexGrid/DrawLos.java
@@ -0,0 +1,24 @@
+package HexGrid;
+
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import HexGrid.HexGrid.DeployedUnit;
+
+public class DrawLos {
+
+	public static void drawLos(Graphics2D g2, ArrayList<DeployedUnit> deployedUnits) {
+		
+		
+	}
+	
+	static void drawLosForUnit() {
+		
+	}
+	
+	static void drawLineBetweenUnits() {
+		
+	}
+	
+	
+}
